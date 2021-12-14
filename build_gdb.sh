e326832778c70304b026605303880f7e89f6972e
Xianting Lu <xialu@microstrategy.com>
2021-12-14 06:01:35 +0000

    add the script to build gdb
    Former-commit-id: e4a6424cb83b8abea0e3a78b38259ec67d75f4d8 [formerly 75e7ba59cfa809d636ce48f4693070087c4ad695]
    Former-commit-id: 2a99c5c112c9c16df4850b42525f4c494e77fd7e

---
+++ build_gdb.sh
@@ -0,0 +1,42 @@
+# ==============================================================================================
+# FILENAME	:	build_gdb.sh
+# AUTHOR	:	Celthi
+# CREATION	:	2021-12-14
+# Script to build the custom gdb with core analyzer.
+# This script will the do the following steps
+# 1. check the gdb version
+# 2. download the gdb 9.2 from gnu.org
+# 3. copy the core analyzer code to the gdb
+# 4. build the gdb
+# ==============================================================================================
+
+PROJECT_FOLDER=$(pwd)
+echo "Current project folder is $PROJECT_FOLDER"
+GDB_VERSION=$(gdb --version | grep "GNU gdb" | awk '{print $8}')
+VERSION_NUM="${GDB_VERSION:0:5}"
+echo $VERSION_NUM
+if [[ "$VERSION_NUM" < "9.2.0" ]]
+then
+    echo "installing gdb 9.2..."
+    temp_folder='gdb_download'
+    mkdir -p /tmp/$temp_folder
+    cd /tmp/$temp_folder
+    gdb_to_install='gdb-9.2'
+    tar_gdb="${gdb_to_install}.tar.gz"
+    if [ ! -f $tar_gdb ]
+    then
+        wget http://ftp.gnu.org/gnu/gdb/$tar_gdb
+    fi
+    if [ ! -d $tar_gdb ]
+    then
+        tar -xf $tar_gdb
+    fi
+    cd $gdb_to_install
+    cp -rLv $PROJECT_FOLDER/gdbplus/gdb-9.2/gdb . 
+    mkdir build
+    cd build
+    PWD=$(pwd)
+
+    $PWD/../configure -disable-binutils --disable-ld --disable-gold --disable-gas --disable-sim --disable-gprof CXXFLAGS='-g' CFLAGS='-g' --prefix=/usr
+    make && make install #&& rm -rf /tmp/$temp_folder
+fi

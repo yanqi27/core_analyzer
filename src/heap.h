9bed4c38808cfcb11f41643710538fbbfa165624
xialu <xialu@microstrategy.com>
2022-05-16 08:05:18 +0000

    switch heap manager on runtime
    Former-commit-id: 2ee17ab34ac9820ac87ea9bfe35af67353d7705d [formerly a5bfb53e5b744d77d22a6b3659c2501aafee63b3]
    Former-commit-id: d135c1d6ae9720d14ea89a5b572fa15a0882e26b

--- src/heap.h
+++ src/heap.h
@@ -1,33 +1,16 @@
-/*
- * heap.h
- *
- *  Created on: Dec 13, 2011
- *      Author: myan
- */
-#ifndef _HEAP_H
-#define _HEAP_H
+// ==============================================================================================
+// FILENAME	:	heap_interface.h
+// AUTHOR	:	Xianting Lu
+// CREATION	:	2022-05-15
+// ==============================================================================================
 
+/** Different programs might use different heap managers
+ * Thi heap interface is the abstract interface for each heap manager
+ * 
+**/
+#ifndef _HEAP_INTERFACE_H
+#define _HEAP_INTERFACE_H
 #include "ref.h"
-
-/*
- * Exposed functions
- */
-extern const char *heap_version(void);
-
-extern bool init_heap(void);
-
-extern bool heap_walk(address_t addr, bool verbose);
-
-extern bool is_heap_block(address_t addr);
-
-extern bool get_heap_block_info(address_t addr, struct heap_block* blk);
-
-extern bool get_next_heap_block(address_t addr, struct heap_block* blk);
-
-extern bool get_biggest_blocks(struct heap_block* blks, unsigned int num);
-
-extern void print_size(size_t sz);
-
 /*
  * Memory usage/leak
  * Aggregated memory is the collection of memory blocks that are reachable from a variable
@@ -47,32 +30,6 @@ struct inuse_block
 };
 
 /*
- * Get all in-use memory blocks
- * 	If param opBlocks is NULL, return number of in-use only,
- * 	otherwise, populate the array with all in-use block info
- */
-extern bool walk_inuse_blocks(struct inuse_block* opBlocks, unsigned long* opCount);
-
-extern struct inuse_block* build_inuse_heap_blocks(unsigned long*);
-extern void free_inuse_heap_blocks(struct inuse_block*, unsigned long);
-
-extern struct inuse_block* find_inuse_block(address_t, struct inuse_block*, unsigned long);
-
-extern bool display_heap_leak_candidates(void);
-
-extern bool biggest_blocks(unsigned int num);
-extern bool biggest_heap_owners_generic(unsigned int num, bool all_reachable_blocks);
-
-extern bool
-calc_aggregate_size(const struct object_reference *ref,
-					size_t var_len,
-					bool all_reachable_blocks,
-					struct inuse_block *inuse_blocks,
-					unsigned long num_inuse_blocks,
-					size_t *aggr_size,
-					unsigned long *count);
-
-/*
  * Histogram of heap blocks
  */
 struct MemHistogram
@@ -84,9 +41,83 @@ struct MemHistogram
 	unsigned long* free_cnt;
 	size_t*        free_bytes;
 };
+
+/**
+ * @brief each heap manager is assigned to an enum
+ * 
+ */
+enum EnumHeapManager {
+    HeapManagerReserved = 0,
+    HeapManagerPtMalloc = 1,
+    HeapManagerTcMalloc = 2,
+	HeapManagerLastOne,
+};
+
+typedef const char * (*HeapVersionFunc)(void);
+typedef bool (*InitHeapFunc)(void);
+typedef  bool (*HeapWalkFunc)(address_t addr, bool verbose);
+typedef bool (*IsHeapBlockFunc)(address_t addr);
+typedef bool (*GetHeapBlockInfoFunc)(address_t addr, struct heap_block* blk);
+typedef bool (*GetNextHeapBlockFunc)(address_t addr, struct heap_block* blk);
+typedef bool (*GetBiggestBlocksFunc)(struct heap_block* blks, unsigned int num);
+typedef void (*PrintSizeFunc)(size_t sz);
+typedef bool (*WalkInuseBlocksFunc)(struct inuse_block* opBlocks, unsigned long* opCount);
+
+typedef struct inuse_block* (*FindInuseBlockFunc)(address_t, struct inuse_block*, unsigned long);
+
+typedef bool (*DisplayHeapLeakCandidatesFunc)(void);
+
+typedef bool (*BiggestBlocksFunc)(unsigned int num);
+typedef bool (*BiggestHeapOwnersGenericFunc)(unsigned int num, bool all_reachable_blocks);
+
+struct CoreAnalyzerHeapInterface {
+    HeapVersionFunc heap_version;
+    InitHeapFunc init_heap;
+    HeapWalkFunc heap_walk;
+    IsHeapBlockFunc is_heap_block;
+    GetHeapBlockInfoFunc get_heap_block_info;
+    GetNextHeapBlockFunc get_next_heap_block;
+    GetBiggestBlocksFunc get_biggest_blocks;
+    /*
+    * Get all in-use memory blocks
+    * 	If param opBlocks is NULL, return number of in-use only,
+    * 	otherwise, populate the array with all in-use block info
+    */
+    WalkInuseBlocksFunc walk_inuse_blocks;
+
+};
+
+// global heaps analyzers
+extern CoreAnalyzerHeapInterface* gCoreAnalyzerHeaps[HeapManagerLastOne];
+extern EnumHeapManager gCurrentHeap;
+#define CA_HEAP gCoreAnalyzerHeaps[gCurrentHeap]
+extern void register_heap_managers();
+
+extern struct inuse_block* build_inuse_heap_blocks(unsigned long*);
+extern void free_inuse_heap_blocks(struct inuse_block*, unsigned long);
+
+extern struct inuse_block* find_inuse_block(address_t, struct inuse_block*, unsigned long);
+
+extern bool display_heap_leak_candidates(void);
+
+extern bool biggest_blocks(unsigned int num);
+extern bool biggest_heap_owners_generic(unsigned int num, bool all_reachable_blocks);
+extern void print_size(size_t sz);
+
+extern bool
+calc_aggregate_size(const struct object_reference *ref,
+					size_t var_len,
+					bool all_reachable_blocks,
+					struct inuse_block *inuse_blocks,
+					unsigned long num_inuse_blocks,
+					size_t *aggr_size,
+					unsigned long *count);
 extern void display_mem_histogram(const char*);
 extern void init_mem_histogram(unsigned int nbuckets);
 extern void release_mem_histogram(void);
 extern void add_block_mem_histogram(size_t, bool, unsigned int);
+
+extern CoreAnalyzerHeapInterface* get_pt_malloc_heap_manager();
+extern CoreAnalyzerHeapInterface* get_tc_malloc_heap_manager();
 
 #endif

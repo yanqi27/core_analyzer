fa2f30252f13817c4b67a95d6b611f9095df9249
Michael Yan <yanqi27@gmail.com>
2022-06-17 11:23:38 -0400

    revert changes to gdb-8.1

--- gdbplus/gdb-8.1/gdb/heapcmd.c
+++ gdbplus/gdb-8.1/gdb/heapcmd.c
@@ -21,55 +21,44 @@ heap_command (const char *args, int from_tty)
 	if (!update_memory_segments_and_heaps())
 		return;
 
-	if (!args)
-		args = "";
 	// remember to resume the current thread/frame
 	scoped_restore_current_thread mythread;
-	gdb::unique_xmalloc_ptr<char> myargs(xstrdup(args));
-	heap_command_impl(myargs.get());
+
+	heap_command_impl(args);
 }
 
 static void
 ref_command (const char *args, int from_tty)
 {
-	if (!args)
-		error_no_arg (_("address"));
-
 	/* We depend on typed segments */
 	if (!update_memory_segments_and_heaps())
 		return;
 
 	// remember to resume the current thread/frame
 	scoped_restore_current_thread mythread;
-	gdb::unique_xmalloc_ptr<char> myargs(xstrdup(args));
-	ref_command_impl(myargs.get());
+
+	ref_command_impl(args);
 }
 
 static void
 pattern_command (const char *args, int from_tty)
 {
-	if (!args)
-		error_no_arg (_("address"));
-
 	/* We depend on typed segments */
 	if (!update_memory_segments_and_heaps())
 		return;
 
 	// remember to resume the current thread/frame
 	scoped_restore_current_thread mythread;
-	gdb::unique_xmalloc_ptr<char> myargs(xstrdup(args));
-	pattern_command_impl(myargs.get());
+
+	pattern_command_impl(args);
 }
 
 static void
-segment_command (const char *args, int from_tty)
+segment_command (const char *arg, int from_tty)
 {
 	if (!update_memory_segments_and_heaps())
 		return;
-	if (!args)
-		args = "";
-	gdb::unique_xmalloc_ptr<char> myargs(xstrdup(args));
-	segment_command_impl(myargs.get());
+	segment_command_impl(arg);
 }
 
 static void
@@ -128,21 +117,23 @@ assign_command (const char *args, int from_tty)
 static void
 unassign_command (const char *args, int from_tty)
 {
-	if (!args)
-		error_no_arg (_("address"));
-
 	// Parse user input options
 	// argument is a list of addresses
-	gdb::unique_xmalloc_ptr<char> myargs(xstrdup(args));
-	char* options[MAX_NUM_OPTIONS];
-	int num_options = ca_parse_options(myargs.get(), options);
-	int i;
-	for (i = 0; i < num_options; i++)
+	if (args)
 	{
-		char* option = options[i];
-		address_t addr = parse_and_eval_address (option);
-		unset_value (addr);
+		char* options[MAX_NUM_OPTIONS];
+		gdb::unique_xmalloc_ptr<char> myargs(xstrdup(args));
+		int num_options = ca_parse_options(myargs.get(), options);
+		int i;
+		for (i = 0; i < num_options; i++)
+		{
+			char* option = options[i];
+			address_t addr = parse_and_eval_address (option);
+			unset_value (addr);
+		}
 	}
+	else
+		error_no_arg (_("address"));
 }
 
 static void
@@ -152,56 +143,21 @@ info_local_command (const char *arg, int from_tty)
 }
 
 static void
-dt_command (const char *args, int from_tty)
+dt_command (const char *arg, int from_tty)
 {
-	if (!args)
+	char* type_or_expr;
+
+	if (!arg)
 		error_no_arg (_("type or variable name"));
-
-	bool print_type = false;
-	size_t min_sz = 0;
-	size_t max_sz = 0;
-
-	gdb::unique_xmalloc_ptr<char> myargs(xstrdup(args));
-	char* options[MAX_NUM_OPTIONS];
-	int num_options = ca_parse_options(myargs.get(), options);
-	int i;
-	for (i = 0; i < num_options; i++) {
-		char* option = options[i];
-		if (*option == '/') {
-			if (strcmp(option, "/size") == 0 || strcmp(option, "/s") == 0) {
-				if (i + 1 >= num_options) {
-					CA_PRINT("size is expected\n");
-					return;
-				}
-				min_sz = ca_eval_address(options[i + 1]);
-				if (i + 2 < num_options) {
-					max_sz = ca_eval_address(options[i + 2]);
-				} else {
-					max_sz = min_sz;
-				}
-				if (min_sz > max_sz) {
-					CA_PRINT("Invalid size arguments\n");
-					return;
-				}
-			} else {
-				CA_PRINT("Invalid option: [%s]\n", option);
-				return;
-			}
-		} else {
-			print_type = true;
-		}
-		break;
-	}
-	if (print_type)
-		print_type_layout (myargs.get());
-	else
-		search_types_by_size(min_sz, max_sz);
+	type_or_expr = strdup(arg);
+	print_type_layout (type_or_expr);
+	free (type_or_expr);
 }
 
 static void
-obj_command (const char *args, int from_tty)
+obj_command (const char *arg, int from_tty)
 {
-	if (!args)
+	if (!arg)
 		error_no_arg (_("type or variable name"));
 
 	/* We depend on typed segments */
@@ -210,33 +166,8 @@ obj_command (const char *args, int from_tty)
 
 	// remember to resume the current thread/frame
 	scoped_restore_current_thread mythread;
-	gdb::unique_xmalloc_ptr<char> myargs(xstrdup(args));
-	char* options[MAX_NUM_OPTIONS];
-	int num_options = ca_parse_options(myargs.get(), options);
-	const char *expr = NULL;
-	bool search_ref = false;
-	bool obj_stats = false;
-	for (int i = 0; i < num_options; i++) {
-		char* option = options[i];
-		if (strcmp(option, "/ref") == 0 || strcmp(option, "/r") == 0) {
-			search_ref = true;
-		} else if (strcmp(option, "/stats") == 0 || strcmp(option, "/s") == 0) {
-			obj_stats = true;
-		} else if (option[0] == '/') {
-			CA_PRINT("invalid option\n");
-			return;
-		} else if (expr) {
-			CA_PRINT("too many expressions\n");
-			return;
-		} else {
-			expr = option;
-		}
-	}
-	if (obj_stats) {
-		display_object_stats();
-	} else {
-		search_cplusplus_objects_and_references(expr, search_ref, false);
-	}
+
+	search_cplusplus_objects_and_references(arg, false);
 }
 
 static void
@@ -328,42 +259,21 @@ display_help_command (const char *arg, int from_tty)
 void
 _initialize_heapcmd (void)
 {
-	add_cmd("ref", class_info, ref_command, _("Search for references to a given object.\n"
-	    "ref <addr_exp>\n"
-		"ref [/thread or /t] <addr_exp> <size> [level]"),
-		&cmdlist);
+	add_cmd("ref", class_info, ref_command, _("Search for references to a given object.\nref <addr_exp>\nref [/thread or /t] <addr_exp> <size> [level]"), &cmdlist);
+	add_cmd("obj", class_info, obj_command, _("Search for object and reference to object of the same type as the input expression\nobj <type|variable>"), &cmdlist);
+	add_cmd("shrobj", class_info, shrobj_command, _("Find objects that currently referenced from multiple threads\nshrobj [tid0] [tid1] [...]"), &cmdlist);
 
-	add_cmd("obj", class_info, obj_command, _("Search for objects that matches the type of the input expression; Stats of all objects\n"
-	    "obj [/stats or /s]\n"
-	    "obj [/ref or /r] <type|variable>"),
-		&cmdlist);
-
-	add_cmd("shrobj", class_info, shrobj_command, _("Find objects that currently referenced from multiple threads\n"
-	    "shrobj [tid0] [tid1] [...]"),
-		&cmdlist);
-
-	add_cmd("heap", class_info, heap_command, _("Heap walk, query, memory usage statistics, leak check, etc.\n"
-		"heap\n"
-		"heap [/block or /b] <addr_exp>\n"
-		"heap [/cluster or /c] <addr_exp>\n"
-		"heap [/usage or /u] <var_exp>\n"
-		"heap [/topblock or /tb] <num>\n"
-		"heap [/topuser or /tu] <num>\n"
+	add_cmd("heap", class_info, heap_command, _("Heap walk, heap data validation, memory usage statistics, etc.\n"
 		"heap [/verbose or /v] [/leak or /l]\n"
-		"heap [/m]"),
+		"heap [/block or /b] [/cluster or /c] <addr_exp>\n"
+		"heap [/usage or /u] <var_exp>\n"
+		"heap [/topblock or /tb] [/topuser or /tu] <num>\n"
+		"heap [/m]\n"),
 		&cmdlist);
 
-	add_cmd("pattern", class_info, pattern_command, _("Reveal memory pattern\n"
-	    "pattern <start> <end>"),
-		&cmdlist);
-
-	add_cmd("segment", class_info, segment_command, _("Display memory segment(s)\n"
-	    "segment [address]"),
-		&cmdlist);
-
-	add_cmd("decode", class_info, decode_command, _("Disassemble current function with detail annotation of object context\n"
-	    "decode %reg=<val> from=<addr> to=<addr>|end"),
-		&cmdlist);
+	add_cmd("pattern", class_info, pattern_command, _("Reveal memory pattern\npattern <start> <end>"), &cmdlist);
+	add_cmd("segment", class_info, segment_command, _("Display memory segments"), &cmdlist);
+	add_cmd("decode", class_info, decode_command, _("Disassemble current function with detail annotation of object context\ndecode %reg=<val> from=<addr> to=<addr>|end"), &cmdlist);
 
 	// Settings
 	add_cmd("shrobj_level", class_info, shrobj_level_command, _("Set/Show the indirection level of shared-object search"), &cmdlist);
@@ -377,9 +287,6 @@ _initialize_heapcmd (void)
 
 	// Misc
 	add_cmd("ca_help", class_info, display_help_command, _("Display core analyzer help"), &cmdlist);
-	add_cmd("dt", class_info, dt_command, _("Display type (windbg style) that matches the input expression or size or size ragne\n"
-	    "dt <type|variable>\n"
-		"dt [/size or /s] <size> [<size-max>]"),
-		&cmdlist);
+	add_cmd("dt", class_info, dt_command, _("Display type (windbg style)\ndt <type|variable>"), &cmdlist);
 	add_cmd("info_local", class_info, info_local_command, _("Display local variables"), &cmdlist);
 }

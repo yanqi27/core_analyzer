c71180a19ce07d5e002a1dc99f59effafed2840c
Michael Yan <yanqi27@gmail.com>
2026-06-04 14:52:42 -0400

    fix break of gdb 9.2

--- gdbplus/gdb-9.2/gdb/x_dep.h
+++ gdbplus/gdb-9.2/gdb/x_dep.h
@@ -13,4 +13,19 @@
 #include "x_common.h"
 #include "gdbcmd.h"
 
+#define ITERATE_OVER_THREADS() iterate_over_threads(thread_tcache, NULL)
+
+#define THREAD_CB_RETURN_TYPE int
+#define THREAD_CB_FUNC(info, data) thread_tcache(struct thread_info *info, void *data)
+#define THREAD_CB_RETURN_CONT 0
+
+#define CA_VALUE_TYPE(value) value_type(value)
+#define CA_VALUE_ADDRESS(value) value_address(value)
+#define CA_TYPE_LENGTH(type) TYPE_LENGTH(type)
+#define CA_TYPE_TARGET_TYPE(type) TYPE_TARGET_TYPE(type)
+
+#define CA_LOOKUP_SYMBOL(name) lookup_symbol(name, 0, VAR_DOMAIN, 0).symbol
+#define CA_LOOKUP_SYMBOL_FUNC(name) lookup_symbol(name, 0, VAR_DOMAIN, 0).symbol
+#define CA_LOOKUP_GLOBAL_SYMBOL(name) lookup_global_symbol(name, 0, VAR_DOMAIN).symbol
+
 #endif // X_DEP_H_
